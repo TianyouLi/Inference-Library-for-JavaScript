edf462b0073ef40e443abf5e1591e8ef8698e2fe
Kanghua Yu <kanghua.yu@intel.com>
2017-04-12 22:52:12 +0800

    bagua-caffe: add getMode and setMode API

--- bagua-caffe/js/caffejs.cpp
+++ bagua-caffe/js/caffejs.cpp
@@ -4,12 +4,38 @@
 #include "caffejs_net.h"
 #include "caffejs_io.h"
 
+
+static NAN_METHOD(getMode) {
+  Nan::HandleScope scope;
+  RETURN(__js(Caffe::mode() == Caffe::CPU ? "CPU" : "GPU"));
+}
+
+static NAN_METHOD(setMode) {
+  Nan::HandleScope scope;
+  String::Utf8Value mode(info[0]->ToString());
+
+  if (strcasecmp(*mode, "CPU") == 0) {
+    Caffe::set_mode(Caffe::CPU);
+  }
+  else if (strcasecmp(*mode, "GPU") == 0) {
+    Caffe::set_mode(Caffe::GPU);
+    Caffe::SetDevice(info.Length() > 1 ? info[1]->IntegerValue() : 0);
+  }
+  else {
+    Nan::ThrowError("Invalid argument");
+  }
+}
+
+
 static NAN_MODULE_INIT(Init) {
   JBlob<float>::Init(target);
   JNet<float>::Init(target);
 
   JMat::Init(target);
   JIO::Init(target);
+
+  Nan::Export(target, "getMode", getMode);
+  Nan::Export(target, "setMode", setMode);
 }
 
 NODE_MODULE(Caffe, Init);
